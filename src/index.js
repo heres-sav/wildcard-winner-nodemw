e080532d3cc995dfcea7d61ea05a1878ac5e6d43
Souvik Dey <souvik.dey@cloudkaptan.com>
2022-11-07 00:05:06 +0530

    INIT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 const app = require("./app");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server,
-  {
-    path: '/flooke-socket',
-    transports: ['polling'],
-    secure: true
-  }
-);
-// const onSocketConnect = require("./services/socket_io/index");
 const envConfig = require("./conf/envConfig");
 const { init } = require("./services/mongoDb/client");
 const constantUtilService = require("./services/utils/constantUtilService");
@@ -24,41 +16,6 @@ server.listen(envConfig.port, async () => {
   } catch (e) {
     logger.error(e);
   }
-})
-
-// SOCKET IO Connection
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("join", async (domain) => {
-    try {
-      socket.join(domain);
-      socket.emit("joined", domain);
-      // eslint-disable-next-line no-param-reassign
-      socket.activeRoom = domain;
-    } catch (e) {
-      console.error(e);
-    }
-  })
-  socket.on("order:add", async (payload) => {
-    console.log("order:add called");
-    const result = await pushOrder(payload)
-    socket.emit("order:status", result);
-    io.to(socket.activeRoom).emit("order:status", result);
-    console.log("order:status emmitted");
-  });
-  socket.on("order:update", async (payload) => {
-    console.log("order:update called");
-    const result = await updateOrder(payload)
-    socket.emit("order:status", result);
-    io.to(socket.activeRoom).emit("order:status", result);
-    console.log("order:status emmitted");
-  });
-  socket.on("order:delete", async (payload) => {
-    console.log("order:delete called");
-    const result = await processOrder(payload)
-    io.to(socket.activeRoom).emit("order:status", result);
-    console.log("order:status emmitted");
-  });
 })
 
 // Unexpected error handler
